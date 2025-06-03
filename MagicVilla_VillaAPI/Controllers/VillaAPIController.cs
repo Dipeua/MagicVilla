3472c990c7ca1a043762eeb08d5ed7c6141a7028
Dipeua <dipeuaberthold@gmail.com>
2025-06-03 23:17:32 +0200

    Adding data

--- MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
-﻿using MagicVilla_VillaAPI.Models;
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
 using MagicVilla_VillaAPI.Models.Dto;
 using Microsoft.AspNetCore.Mvc;
 
@@ -12,11 +13,6 @@ public class VillaAPIController : Controller
     [HttpGet]
     public IEnumerable<VillaDTO> GetVillas()
     {
-        return new List<VillaDTO>
-        {
-            new VillaDTO { Id = 1, Name = "Villa 1" },
-            new VillaDTO { Id = 2, Name = "Villa 2" },
-            new VillaDTO { Id = 3, Name = "Villa 3" }
-        };
+        return VillaStore.VillaList;
     }
 }
