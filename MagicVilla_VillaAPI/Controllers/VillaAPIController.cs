bdd534beb2a366acc1595b2d7c258b7054481372
Dipeua <dipeuaberthold@gmail.com>
2025-06-03 23:07:21 +0200

    Fisrt api route

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using MagicVilla_VillaAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers;
+
+[ApiController]
+[Route("api/VillaAPI")]
+public class VillaAPIController : Controller
+{
+    [HttpGet]
+    public IEnumerable<Villa> GetVillas()
+    {
+        return new List<Villa>
+        {
+            new Villa { Id = 1, Name = "Villa 1" },
+            new Villa { Id = 2, Name = "Villa 2" },
+            new Villa { Id = 3, Name = "Villa 3" }
+        };
+    }
+}
