cb54182ec7304378917d61203c587b7b7d43f4bc
Dipeua <dipeuaberthold@gmail.com>
2025-06-05 18:41:10 +0200

    Log files using Serilog

--- MagicVilla_VillaAPI/Program.cs
+++ MagicVilla_VillaAPI/Program.cs
@@ -1,6 +1,14 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.File("Logs/Villalogs.txt", rollingInterval:RollingInterval.Day)
+    .CreateLogger();
+builder.Host.UseSerilog();
 
 builder.Services.AddControllers(options =>
 {
